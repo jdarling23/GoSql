9e50195cef725600bf56ea30f8c6c97c4035d5a7
James Darling <jamesdarling2@gmail.com>
2018-05-14 21:19:05 -0500

    Added in command handlers and place holder for message parser

--- GoSql.go
+++ GoSql.go
@@ -4,8 +4,25 @@ import (
 	"fmt"
 	"net"
 	"os"
-	"time"
 )
+
+var commandHandlers = map[string]interface{}{
+	"PUT":       handlePut,
+	"GET":       handleGet,
+	"GETLIST":   handleGetlist,
+	"PUTLIST":   handlePutlist,
+	"INCREMENT": handleIncrement,
+	"APPEND":    handleAppend,
+	"DELETE":    handleDelete,
+	"STATS":     handleStats,
+}
+
+type message struct {
+	command   string
+	key       string
+	value     string
+	valueType string
+}
 
 func main() {
 
@@ -23,24 +40,59 @@ func main() {
 		if err != nil {
 			continue
 		}
-		go handleClient(conn)
+		go parseMessage(conn)
 	}
 }
 
-func getDatabase(message string) {
+//Database Helper Functions
+func getDatabase() {
 	//This will return the json file that is the database. The queries will happen against the object that is returned
 }
 
-func handleClient(conn net.Conn) {
+func parseMessage(conn net.Conn) {
 	defer conn.Close()
-	daytime := time.Now().String()
-	conn.Write([]byte(daytime)) // don't care about return value
-	// we're finished with this client
+	//var mess message
+	//mess = conn.Read
+	//fmt.Println(mess)
 }
+
 func checkError(err error) {
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Fatal error: %s", err.Error())
 		os.Exit(1)
 		//Need a function here to write the database back to the file
 	}
+}
+
+//Command Functions
+func handlePut() {
+
+}
+
+func handleGet() {
+
+}
+
+func handleGetlist() {
+
+}
+
+func handlePutlist() {
+
+}
+
+func handleIncrement() {
+
+}
+
+func handleAppend() {
+
+}
+
+func handleDelete() {
+
+}
+
+func handleStats() {
+
 }
